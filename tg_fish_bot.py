6956822ef7b5d235430884a821b6c16ba626ff3d
killerarz <milov52@gmail.com>
2023-01-12 12:04:42 +0300

    runned state machine

---
+++ tg_fish_bot.py
@@ -0,0 +1,71 @@
+import os
+import logging
+import redis
+from dotenv import load_dotenv
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup
+
+from telegram.ext import Filters, Updater
+from telegram.ext import CallbackQueryHandler, CommandHandler, MessageHandler
+
+_database = None
+
+
+
+def echo(bot, update):
+    users_reply = update.message.text
+    update.message.reply_text(users_reply)
+    return "ECHO"
+
+def handle_users_reply(bot, update):
+    db = get_database_connection()
+
+    if update.message:
+        user_reply = update.message.text
+        chat_id = update.message.chat_id
+    elif update.callback_query:
+        user_reply = update.callback_query.data
+        chat_id = update.callback_query.message.chat_id
+    else:
+        return
+
+    if user_reply == '/start':
+        user_state = 'START'
+    else:
+        user_state = db.get(chat_id).decode('utf-8')
+
+
+    states_functions = {
+        'START': start,
+        'ECHO': echo
+    }
+
+    state_handler = states_functions[user_state]
+    try:
+        next_state = state_handler(bot, update)
+        db.set(chat_id, next_state)
+    except Exception as err:
+        print(err)
+
+def get_database_connection():
+    global _database
+    if _database is None:
+        database_password = os.environ.get("DATABASE_PASSWORD")
+        database_host = os.environ.get("DATABASE_HOST")
+        database_port = os.environ.get("DATABASE_PORT")
+
+        _database = redis.Redis(host=database_host, port=database_port, password=database_password)
+    return _database
+
+
+if __name__ == '__main__':
+    load_dotenv()
+    token = os.environ.get("TELEGRAM_TOKEN")
+
+    updater = Updater(token)
+    dispatcher = updater.dispatcher
+    dispatcher.add_handler(CallbackQueryHandler(handle_users_reply))
+    dispatcher.add_handler(MessageHandler(Filters.text, handle_users_reply))
+    dispatcher.add_handler(CommandHandler('start', handle_users_reply))
+
+    updater.start_polling()
+
